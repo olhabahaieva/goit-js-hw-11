06e1c3962740b7b9388df5859e16e6021c1a5850
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-04-29 20:17:54 +0300

    added createMarkup function

--- src/index.js
+++ src/index.js
@@ -36,30 +36,39 @@ async function getImages(images) {
   } catch (error) {
     console.error(error);
   }
+  return response;
 }
 
 function createMarkup(response) {
   return response
     .map(
-      data => `
-      <div class="photo-card">
-      <img src="" alt="" loading="lazy" />
-      <div class="info">
-        <p class="info-item">
-          <b>Likes</b>
-        </p>
-        <p class="info-item">
-          <b>Views</b>
-        </p>
-        <p class="info-item">
-          <b>Comments</b>
-        </p>
-        <p class="info-item">
-          <b>Downloads</b>
-        </p>
-      </div>
-    </div>
-  `
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `
+        <div class="photo-card">
+          <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+          <div class="info">
+            <p class="info-item">
+              <b>Likes:</b> ${likes}
+            </p>
+            <p class="info-item">
+              <b>Views:</b> ${views}
+            </p>
+            <p class="info-item">
+              <b>Comments:</b> ${comments}
+            </p>
+            <p class="info-item">
+              <b>Downloads:</b> ${downloads}
+            </p>
+          </div>
+        </div>
+      `
     )
     .join('');
 }
